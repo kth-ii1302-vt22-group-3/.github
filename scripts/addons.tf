4f0de5bf67d13f162ef955e217fbb22402cebe7a
arifjehoh <arifjeh.oh@gmail.com>
2022-03-26 23:45:11 +0100

    #7: Remove database in web

--- scripts/addons.tf
+++ scripts/addons.tf
@@ -5,9 +5,3 @@ resource "heroku_addon" "api" {
   app_id = heroku_app.api.id
   plan   = "heroku-postgresql:hobby-dev"
 }
-
-# Staging database
-resource "heroku_addon" "web" {
-  app_id = heroku_app.web.id
-  plan   = "heroku-postgresql:hobby-dev"
-}
